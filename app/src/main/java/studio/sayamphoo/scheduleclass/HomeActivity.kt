71496bd65190c3e6040f0e21829e5e2040ae50fc
Sayamphoo <sayamphoo761@gmail.com>
2020-01-12 19:21:08 +0700

    App Update

--- app/src/main/java/studio/sayamphoo/scheduleclass/HomeActivity.kt
+++ app/src/main/java/studio/sayamphoo/scheduleclass/HomeActivity.kt
@@ -1,15 +1,12 @@
 package studio.sayamphoo.scheduleclass
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        startActivity(Intent(this,HomeActivity::class.java))
+        setContentView(R.layout.activity_home)
     }
 }
